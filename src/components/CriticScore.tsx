fafa91aaf99ca54b78fe28295e22684d15b924da
mohammed zakout <mohammedzakkh@gmail.com>
2023-05-30 17:04:20 +0300

    Desplaying critic score

--- src/components/CriticScore.tsx
+++ src/components/CriticScore.tsx
@@ -1,8 +1,9 @@
 import { Badge } from '@chakra-ui/react'
-import React from 'react'
+
 interface Props {
   score: number
 }
+
 const CriticScore = ({ score }: Props) => {
   const color = score > 75 ? 'green' : score > 60 ? 'yellow' : ''
   return (
